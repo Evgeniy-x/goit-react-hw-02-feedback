a936eacb939346d3f71cbb2951c4c7d72e5289a5
Evgeniy <yaremenko.x@gmail.com>
2023-06-05 19:07:44 +0300

    Update Notification.jsx

--- src/components/Feedback/Notification.jsx
+++ src/components/Feedback/Notification.jsx
@@ -4,8 +4,8 @@ const Notification = ({ message }) => {
   return <p>{message}</p>;
 };
 
+export default Notification;
+
 Notification.propTypes = {
   message: PropTypes.string.isRequired,
 };
-
-export default Notification;
